a4477a00968a13b5c733a42e7096514da1a2fa44
Blank Xiao <blankxiao0810@gmail.com>
2024-10-03 21:05:03 +0800

    chore: rm log

--- src/plugins/remarkMermaidPlugin.js
+++ src/plugins/remarkMermaidPlugin.js
@@ -2,8 +2,6 @@ import { visit } from 'unist-util-visit'
 
 export function mermaid() {
   return function (tree, { data }) {
-    console.log(tree)
-    console.log(data)
     visit(tree, 'code', (node) => {
       if (node.lang === 'mermaid') {
         node.type = 'html'
